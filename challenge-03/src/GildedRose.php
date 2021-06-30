10197a9bf59689ab075b205fc71abd3a3f0627c7
juan jose vilchez barzola <juan_vb_1998@hotmail.com>
2021-06-29 21:54:10 -0500

    Challenge 3

--- challenge-03/src/GildedRose.php
+++ challenge-03/src/GildedRose.php
@@ -23,30 +23,39 @@ class GildedRose
 
     public function tick()
     {
-        if ($this->name != 'Aged Brie' and $this->name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if ($this->name != 'Aged Brie' and $this->name != 'Backstage passes to a TAFKAL80ETC concert' and $this->name != 'Conjured Mana Cake') {
             if ($this->quality > 0) {
                 if ($this->name != 'Sulfuras, Hand of Ragnaros') {
                     $this->quality = $this->quality - 1;
+
                 }
             }
         } else {
-            if ($this->quality < 50) {
-                $this->quality = $this->quality + 1;
-
-                if ($this->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                    if ($this->sellIn < 11) {
-                        if ($this->quality < 50) {
-                            $this->quality = $this->quality + 1;
+            if($this->name != 'Conjured Mana Cake'){
+                if ($this->quality < 50) {
+                    $this->quality = $this->quality + 1;
+                    if ($this->name == 'Backstage passes to a TAFKAL80ETC concert') {
+                        if ($this->sellIn < 11) {
+                            if ($this->quality < 50) {
+                                $this->quality = $this->quality + 1;
+                            }
+                        }
+                        if ($this->sellIn < 6) {
+                            if ($this->quality < 50) {
+                                $this->quality = $this->quality + 1;
+                            }
                         }
                     }
-                    if ($this->sellIn < 6) {
-                        if ($this->quality < 50) {
-                            $this->quality = $this->quality + 1;
-                        }
+                }
+            }else{
+                if ($this->quality > 0) {
+                    if ($this->name != 'Sulfuras, Hand of Ragnaros') {
+                        $this->quality = $this->quality - 2;
                     }
                 }
             }
         }
+
 
         if ($this->name != 'Sulfuras, Hand of Ragnaros') {
             $this->sellIn = $this->sellIn - 1;
@@ -71,3 +80,4 @@ class GildedRose
         }
     }
 }
+
